9097dd5ba74932fe62621fd5680b04638161b9c4
anurag pal <palanurag083@gmail.com>
2026-03-15 16:55:58 +0530

    setup backend architecture and database connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const http = require("http");
+const { Server } = require("socket.io");
+const connectiondb = require("./config/db");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
 const app = express();
 
-app.get("/", (req , res ) => {
-    res.send("sync pad server running ");
+connectiondb();
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const servers = http.createServer(app);
+
+const io = new Server(servers , {
+    cors:{
+        origin : "*"
+    }
 });
 
-app.listen(8080 , ()=>{
-    console.log("server is running on port 8080")
+io.on("connection", (socket) => {
+    console.log("user connected", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("user dissconnected");
+    });
+});
+
+app.get("/", (req, res) => {
+  res.send("SyncPad API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+servers.listen(PORT , ()=>{
+    console.log(`server is running on port ${PORT}`);
 });
